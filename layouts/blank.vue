d90d397db60a4d582d7266a91bb7c7924bb54903
FinGet <zhangbiaoFT@163.com>
2019-08-06 10:23:05 +0800

    加一个错误页面

---
+++ layouts/blank.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h1>404页面不存在</h1>
+    <nuxt-link to="/">回 首 页</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'blank' // 你可以为错误页面指定自定义的布局
+}
+</script>
+<style lang="less" scoped>
+.container{
+ .flex-direction--column;
+ height: 500px;
+ align-items: center;
+ h1{
+   color: @redColor;
+   font-size: 30px;
+ }
+ a{
+   text-decoration: underline;
+ }
+}
+</style>
